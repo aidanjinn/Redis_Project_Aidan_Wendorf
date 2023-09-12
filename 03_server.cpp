de8f3a06ba951ba77630bdb7e29b5f2a13fa29b6
Aidan Wendorf <96208746+aidanjinn@users.noreply.github.com>
2023-09-12 12:11:11 -0500

    Version 1.1 First Implementation of Protocol Parsing; Handling multiple requests from the client

--- 03_server.cpp
+++ 03_server.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,24 +15,102 @@ static void msg(const char *msg) {
 
 static void die(const char *msg) {
     int err = errno;
-    fprintf(stderr, "[%d} %s\n", err, msg);
+    fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
 }
 
-static void do_something(int connfd) {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        msg("read() error");
-        return;
+const size_t k_max_msg = 4096;
+
+//read from the kernal until it gets exactly n bytes
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
     }
-    printf("client says: %s\n", rbuf);
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+    return 0;
 }
+
+//writes n bytes to the kernal
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+//parser
+static int32_t one_request(int connfd) {
+    //4 byte header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4); //assume little endian
+    if (len > k_max_msg) {
+        msg("message too long");
+        return -1;
+    }
+
+    //request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    //do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    //reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+    /*
+     * For convenience we put a limit on the max request size and use a buffer large enough to
+     * hold one request.
+     */
+}
+
+
+/*
+ * We be using the following protocol to split requests apart from the TCP byte stream
+ * giving us the ability to send multiple requests in a single TCP packet
+ * |Len|msg1|len|msg2|more....|
+ * The protocol consists of 2 parts a 4-byte little endian indicating the length of the requrest
+ * and the var length request
+ */
 
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0 ) {
+        die("socket()");
+    }
+
+
     int val = 1;
     //used to configure various options for a socket
     //this call enables the SO_REUSEADDR option
@@ -39,10 +118,10 @@ int main() {
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
     //bind, syntax for IPV4
-    struct sockaddr_in addr;
+    struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(0);
+    addr.sin_addr.s_addr = ntohl(0); // wildcard address 0.0.0.0
     int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
     if (rv) {
         die("bind()");
@@ -63,12 +142,14 @@ int main() {
             continue; //error
         }
 
-        do_something(connfd);
+        //only serves one client connection at once
+        while (true) {
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
         close(connfd);
     }
-
-
-
-
     return 0;
 }
